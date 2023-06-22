58cd2cdbb20c20675991dbaefdfce586ed7a06b4
shutka007 <113580732+shutka007@users.noreply.github.com>
2023-06-23 08:18:34 +0900

    init: new provider

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,7 +3,9 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
+use App\Providers\UserProvider;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Auth;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -22,5 +24,13 @@ class AuthServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+        $this->registerPolicies();
+        
+        // you can choose a different name
+        Auth::provider('user', function ($app, array $config) {
+            // Return an instance of Illuminate\Contracts\Auth\UserProvider...
+ 
+            return new UserProvider($app->make('user.connection'));
+        });
     }
 }
