d50e0d341741d916a41297345a5210897def7603
shutka007 <113580732+shutka007@users.noreply.github.com>
2023-06-23 07:46:54 +0900

    fix

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,8 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->string('password');
+            $table->string('password'); 
+            $table->softDeletes();
             $table->timestamps();
         });
     }
--- database/migrations/2023_06_16_064042_create_products_table.php
+++ database/migrations/2023_06_16_064042_create_products_table.php
@@ -18,7 +18,6 @@ return new class extends Migration
             $table->integer('price');
             $table->softDeletes();
             $table->foreignId('categories_id')->constrained();
-            
             $table->timestamps();
         });
     }
