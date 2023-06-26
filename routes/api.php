7f589c67df0385781f50f865cb9474a9e0c73621
shutka007 <113580732+shutka007@users.noreply.github.com>
2023-06-26 15:29:24 +0900

    routes auth check

--- routes/api.php
+++ routes/api.php
@@ -19,20 +19,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('categories', [CategoryController::class, 'store']);
-Route::post('categories/(id)/restore', [CategoryController::class, 'restore']);
-Route::patch('categries/{id}/update', [CategoryController::class, 'update']);
-Route::delete('categories', [CategoryController::class, 'index']);
-Route::get('categories', [CategoryController::class, 'index']);
-Route::get('categories/{id}', [CategoryController::class, 'show']);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('categories', [CategoryController::class, 'store']);
+    Route::post('categories/(id)/restore', [CategoryController::class, 'restore']);
+    Route::patch('categries/{id}/update', [CategoryController::class, 'update']);
+    Route::delete('categories', [CategoryController::class, 'index']);
+    Route::get('categories', [CategoryController::class, 'index']);
+    Route::get('categories/{id}', [CategoryController::class, 'show']);
 
-Route::post('products', [ProductController::class, 'store']);
-Route::post('products/(id)/restore', [ProductController::class, 'restore']);
-Route::patch('products/{id}/update', [ProductController::class, 'update']);
-Route::delete('products', [ProductController::class, 'index']);
-Route::get('products', [ProductController::class, 'index']);
-Route::get('products/{id}', [ProductController::class, 'show']);
-
+    Route::post('products', [ProductController::class, 'store']);
+    Route::post('products/(id)/restore', [ProductController::class, 'restore']);
+    Route::patch('products/{id}/update', [ProductController::class, 'update']);
+    Route::delete('products', [ProductController::class, 'index']);
+    Route::get('products', [ProductController::class, 'index']);
+    Route::get('products/{id}', [ProductController::class, 'show']);
+});
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function () {
